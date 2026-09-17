53ba1d41a0db14175d2d3dd185833249ebf1b863
felipenakai-maker <felipenakai@edu.senai.br>
2026-09-17 07:47:28 -0300

    Adicionar arquivos de projeto.

---
+++ ISalvavel.cs
@@ -0,0 +1,24 @@
+﻿namespace ClasseAbstrata
+{
+    internal interface ISalvavel
+    {
+        void Salvar();
+    }
+
+    public class Documento : ISalvavel
+    {
+        public void Salvar()
+        {
+            Console.WriteLine("SALVANDO O DOCUMENTO EM FORMATO.DOC");
+        }
+    }
+
+    public class Foto : ISalvavel
+    {
+        public void Salvar()
+        {
+            Console.WriteLine("SALVANDO A FOTO EM FORMATO .JPG");
+        }
+    }
+
+}
